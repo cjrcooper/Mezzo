f8e24c97e478082825eb24dc8c6933f2937401b0
cjrcooper <Coops@Christophers-MacBook-Pro-2.local>
2016-05-12 11:24:49 +1000

    username stuff

--- db/migrate/20160430015327_add_username_to_users.rb
+++ db/migrate/20160430015327_add_username_to_users.rb
@@ -1,4 +1,5 @@
 class AddUsernameToUsers < ActiveRecord::Migration
+
   def change
     add_column :users, :username, :string # add a new column
     add_index :users, :username, unique: true #indexes the username column and makes sure it is unique

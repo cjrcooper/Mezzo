34700f943278c8063ba68c6d586fcf69c6178bc4
cjrcooper <Coops@Christophers-MacBook-Pro-2.local>
2016-05-12 11:22:16 +1000

    fUCKED UP INdexes

--- db/migrate/20160430015327_add_username_to_users.rb
+++ db/migrate/20160430015327_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string # add a new column
-    add_index :users, username, unique: true #indexes the username column and makes sure it is unique
+    add_index :users, :username, unique: true #indexes the username column and makes sure it is unique
   end
 end
